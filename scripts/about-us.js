0f28f8612b8e25cbc2fdd0cb1bbcbdecdfff95b8
IsaacAmp24 <ampudiaflores@gmailcom>
2024-04-17 05:20:55 -0500

    feat(toolbar): seccion toolbar aniadido y otros disenios

---
+++ scripts/about-us.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("fade-in");
+            }
+        });
+    });
+
+    const aboutSection = document.querySelector("#product");
+    observer.observe(aboutSection);
+});
